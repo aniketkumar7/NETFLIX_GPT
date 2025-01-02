0ca347515cc4e431cfd9cd6b3b181d49794e15ac
Aniket Kumar <aniketkumar639597@gmail.com>
2025-01-02 21:15:14 +0530

    Update MovieList.js

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -3,12 +3,19 @@ import React from "react";
 
 const MovieList = ({ title, movies }) => {
   return (
-    <div className="px-6 py-2">
-      <h1 className="text-lg md:text-3xl mt-3 font-semibold py-4 text-white">{title}</h1>
-      <div id="scrollable" className="flex overflow-x-scroll overflow-y-hidden">
-        <div className="flex">
-          {movies?.map((movie) => (
-            <MovieCard key={movie.id} posterPath={movie.poster_path} />
+    <div className="lg:py-5 md:py-4 sm:py-3 py-2.5">
+      <span className="lg:text-2xl md:text-xl sm:text-base text-base font-semibold md:pl-3 pl-2 lg:pl-4">
+        {title}
+      </span>
+      <div className="flex overflow-x-auto no-scrollbar  lg:pt-5 md:pt-4 sm:pt-2.5 pt-2.5">
+        <div className="flex lg:gap-4 md:gap-3 gap-2 sm:gap-2">
+          {movie?.map((movie) => (
+            <Link to={"/movieinfo/" + movie?.id}>
+            <MovieCard
+              key={movie?.id}
+              poster={movie?.poster_path || movie?.profile_path} title={movie?.title || movie?.original_name}
+            />
+            </Link>
           ))}
         </div>
       </div>
