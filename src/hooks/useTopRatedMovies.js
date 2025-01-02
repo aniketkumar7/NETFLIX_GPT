59785e094c30e46856bf01fbeb32b5c94d513e39
Aniket Kumar <aniketkumar639597@gmail.com>
2025-01-02 21:08:35 +0530

    Update useTopRatedMovies.js

--- src/hooks/useTopRatedMovies.js
+++ src/hooks/useTopRatedMovies.js
@@ -11,7 +11,7 @@ const useTopRatedMovies = () => {
 
   const getTopRatedMovies = async () => {
     const data = await fetch(
-      "https://api.themoviedb.org/3/movie/top_rated?&page=1",
+      "https://api.themoviedb.org/3/tv/top_rated",
       API_OPTIONS
     );
     const json = await data.json();
