b8694c6e7a3145c6ca307bcdfca0d414f640cff8
Aniket Kumar <aniketkumar639597@gmail.com>
2025-01-02 20:57:44 +0530

    Update constant.js

--- src/Utils/constant.js
+++ src/Utils/constant.js
@@ -1,3 +1,5 @@
+const TMDB_API_KEY = process.env.REACT_APP_TMDB_KEY;
+
 export const LOGO =
   "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 
@@ -8,7 +10,7 @@ export const API_OPTIONS = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1Ni'
+    Authorization: `Bearer ${TMDB_API_KEY}`,
   },
 };
 
